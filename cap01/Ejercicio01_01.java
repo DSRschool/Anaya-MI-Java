a479e313d24102aea6c40a2423466e0a15d3c3b1
Mariona DSR <mariona.nadal@dsrroma.es>
2021-03-29 20:39:44 +0200

    5. Ejercicios 1.1 y 1.2 resueltos

---
+++ cap01/Ejercicio01_01.java
@@ -0,0 +1,14 @@
+/* Ejercicio 1.1 - Escribe un programa que imprima por pantalla la frase 
+ * “Estoy aprendiendo a programar bien en Java”. 
+ */
+
+// La clase debe llamarse igual que el fichero
+public class Ejercicio01_01 {
+	
+	// el método main es el que se ejecuta al ejecutar una clase
+	public static void main(String[] args) {
+		// System.out.print o System.out.println escriben por la salida estándar
+		// que es la consola
+		System.out.println("Estoy aprendiendo a programar bien en Java");
+	}
+}
---
+++ cap01/Ejercicio01_02.java
@@ -0,0 +1,12 @@
+/* Ejercicio 1.2 - Escribe un programa que imprima por pantalla 
+ * estas dos frases: “La vida es tan buena maestra,” y 
+ * “que si no aprendes la lección te la repite.”.
+ */
+
+public class Ejercicio01_02 {
+	
+	public static void main(String[] args) {
+		System.out.println("La vida es tan buena maestra,");
+		System.out.println("que si no aprendes la lección te la repite.");
+	}
+}
