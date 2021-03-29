3097cf5af1784efcbafe10da49ef912523a90801
Mariona DSR <mariona.nadal@dsrroma.es>
2021-03-29 20:00:30 +0200

    3. Qué puede salir mal con el método main de Java

---
+++ cap01/Ejemplo01_01b.java
@@ -0,0 +1,5 @@
+public clas Ejemplo01_01b {
+    public static void main(String[] args) {
+ 
+    }
+}
---
+++ cap01/Ejemplo01_01c.java
@@ -0,0 +1,5 @@
+public class Ejemplo01_01c {
+    public staic void main(String[] args) {
+ 
+    }
+}
