542b835dee0093ec7552a691fdeeea5bfaf4a56f
Mariona DSR <mariona.nadal@dsrroma.es>
2021-03-30 14:50:05 +0200

    6. Operadores relacionales

---
+++ cap02/Ejemplo02_06.java
@@ -0,0 +1,16 @@
+public class Ejemplo02_06 {
+	public static void main(String[] args) {
+		int x = 3;
+		int y = 3;
+		System.out.println("x: " + x);
+		System.out.println("y: " + y);
+		System.out.println("x == y? " + (x == y) );
+		System.out.println("x != y? " + (x != y) );
+		String a = new String("hola");
+		String b = new String("hola");
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+		System.out.println("a == b? " + (a == b) );
+		System.out.println("a != b? " + (a != b) );
+	}
+}
