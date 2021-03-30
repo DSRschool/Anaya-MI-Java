2bd7b24c5b712e9eef5ba1eace1ba688aa057035
Mariona DSR <mariona.nadal@dsrroma.es>
2021-03-30 14:47:38 +0200

    5. Operadores aritméticos

---
+++ cap02/Ejemplo02_05.java
@@ -0,0 +1,10 @@
+public class Ejemplo02_05 {
+	public static void main(String[] args) {
+		int n = 7;
+		System.out.println("1) n: " + n);
+		System.out.println("2) ++n: " + ++n);
+		System.out.println("3) n: " + n);
+		System.out.println("4) n++: " + n++);
+		System.out.println("5) n: " + n);
+	}
+}
