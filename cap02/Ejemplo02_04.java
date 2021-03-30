9076a930b164c5de4c2bb32eb60b79b5b236cf5d
Mariona DSR <mariona.nadal@dsrroma.es>
2021-03-30 14:45:27 +0200

    5. Operadores aritméticos

---
+++ cap02/Ejemplo02_04.java
@@ -0,0 +1,10 @@
+public class Ejemplo02_04 {
+	public static void main(String[] args) {
+		int divisionEntera = 7 / 2;
+		int modulo = 7 % 2;
+		double divisionReal = 7.0 / 2.0;
+		System.out.println("División Entera 7/2: " + divisionEntera);
+		System.out.println("Módulo 7%2: " + modulo);
+		System.out.println("División Real 7.0/2.0: " + divisionReal); 
+	}
+}
