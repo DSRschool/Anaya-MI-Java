c088eeb7d31945ed4521a26e223e23a88b192dfd
Mariona DSR <mariona.nadal@dsrroma.es>
2021-04-02 11:18:25 +0200

    7. Caracteres especiales

---
+++ cap04/Ejemplo04_08.java
@@ -0,0 +1,7 @@
+public class Ejemplo04_08 {
+	public static void main(String[] args) {
+		System.out.println(
+			"Debe copiar el texto \"I'm happy\" dentro del fichero:\n" +
+			"tC:\\app\\nube\\recursos\\textos.txt");
+	}
+}
