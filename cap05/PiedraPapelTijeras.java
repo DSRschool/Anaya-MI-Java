cd7cee6e889d112b0260572f614ce015fe11751f
Mariona DSR <mariona.nadal@dsrroma.es>
2021-04-03 20:39:17 +0200

    1. Preparar el esqueleto y las constantes necesarias

---
+++ cap05/PiedraPapelTijeras.java
@@ -0,0 +1,15 @@
+/* Proyecto – Vamos a jugar a piedra-papel-tijeras contra el ordenador. 
+ * Tendrás que explicarle al jugador cómo se juega, pedirle que nos dé su jugada 
+ * (Piedra, papeL, Tijeras, Salir), generar una jugada aleatoria para el ordenador 
+ * y decidir quien ha ganado.
+ */
+
+public class PiedraPapelTijeras {
+	private static final String PIEDRA = "P";
+	private static final String PAPEL = "L";
+	private static final String TIJERAS = "T";
+	private static final String SALIR = "S";
+
+	public static void main(String[] args) {
+	}
+}
