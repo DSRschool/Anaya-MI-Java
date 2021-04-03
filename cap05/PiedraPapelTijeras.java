ec53372086f5af97ae524788abcb75dbecd8daa5
Mariona DSR <mariona.nadal@dsrroma.es>
2021-04-03 20:40:52 +0200

    2. Mostrar instrucciones al usuario

--- cap05/PiedraPapelTijeras.java
+++ cap05/PiedraPapelTijeras.java
@@ -10,6 +10,16 @@ public class PiedraPapelTijeras {
 	private static final String TIJERAS = "T";
 	private static final String SALIR = "S";
 
+	// Mensajes al usuario
+	private static final String BIENVENIDA = 
+		"¡Bienvenido al juego Piedra-papel-tijeras!";
+	private static final String PEDIR_JUGADA = "¿Cuál es tu jugada? " 
+		+ PIEDRA + " (piedra), " + PAPEL + " (papel), " 
+		+ TIJERAS + " (tijeras) o " + SALIR + " (salir)";
+
 	public static void main(String[] args) {
+		// Instrucciones
+		System.out.println(BIENVENIDA);
+		System.out.println(PEDIR_JUGADA);
 	}
 }
