ada22b66586967d2c8659cd2f54229388fcd6e6f
Mariona DSR <mariona.nadal@dsrroma.es>
2021-04-08 17:50:21 +0200

    8. Los objetos de sOOPer

---
+++ cap07/src/sooper/Pedido.java
@@ -0,0 +1,42 @@
+package sooper;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Pedido implements IPedido {
+
+	private String referencia;
+	private Set<IContenedor> contenedores;
+
+	public Pedido(String referencia) {
+		this.referencia = referencia;
+		this.contenedores = new HashSet<>();
+	}
+
+	@Override
+	public String getReferencia() {
+		return referencia;
+	}
+
+	@Override
+	public Set<IProducto> getProductos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<IContenedor> getContenedores() {
+		return contenedores;
+	}
+
+	@Override
+	public void addContenedor(IContenedor contenedor) {
+		contenedores.add(contenedor);
+	}
+
+	@Override
+	public IContenedor addProducto(IProducto producto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
