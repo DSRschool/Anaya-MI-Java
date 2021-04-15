b2d68126ddbac012de590e960cc7641c5717ac06
Mariona DSR <mariona.nadal@dsrroma.es>
2021-04-15 18:19:42 +0200

    0. Base de capítulo 7

---
+++ cap08/src/sooper/IContenedor.java
@@ -0,0 +1,26 @@
+package sooper;
+
+import java.util.Set;
+
+import sooper.enums.TipoContenedor;
+
+public interface IContenedor {
+	
+	String getReferencia();
+
+	int getVolumen();
+
+	int volumenDisponible();
+
+	int getResistencia();
+	
+	Set<IProducto> getProductos();
+	
+	TipoContenedor getTipo();
+
+	boolean meter(IProducto producto);
+
+	boolean resiste(IProducto producto);
+	
+	int getSuperficie();
+}
---
+++ cap08/src/sooper/enums/Categoria.java
@@ -0,0 +1,5 @@
+package sooper.enums;
+
+public enum Categoria {
+	ALIMENTACION, DROGUERIA, HIGIENE, MASCOTAS;
+}
