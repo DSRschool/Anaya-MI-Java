58cbb3faa6a2cc18ef9e1cce3a2ef03e5ec934ab
Mariona DSR <mariona.nadal@dsrroma.es>
2021-04-15 18:27:52 +0200

    1. Clase Producto

---
+++ cap08/src/sooper/Producto.java
@@ -0,0 +1,41 @@
+package sooper;
+
+public abstract class Producto implements IProducto {
+
+	private String referencia;
+	private int peso;
+	private int volumen;
+
+	public Producto(String referencia, int peso, int volumen) {
+		this.referencia = referencia;
+		this.peso = peso;
+		this.volumen = volumen;
+	}
+
+	@Override
+	public String getReferencia() {
+		return referencia;
+	}
+
+	@Override
+	public int getPeso() {
+		return peso;
+	}
+
+	@Override
+	public int getVolumen() {
+		return volumen;
+	}
+
+	@Override
+	public boolean tengoEspacio(IContenedor contenedor) {
+		// TODO Apéndice de método generado automáticamente
+		return false;
+	}
+
+	@Override
+	public void meter(IContenedor contenedor) {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+}
