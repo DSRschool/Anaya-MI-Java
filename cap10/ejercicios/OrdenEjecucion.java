6ee8b4228e90811a932254a88bba2a69f99c5275
Mariona DSR <mariona.nadal@dsrroma.es>
2021-04-28 19:59:26 +0200

    17. Ejercicio 10.4 - Solución

--- cap10/ejercicios/OrdenEjecucion.java
+++ cap10/ejercicios/OrdenEjecucion.java
@@ -12,7 +12,7 @@ public class OrdenEjecucion {
 	// 1 - Si metodoUno lanza AException: A-B-D-G-H-I
 	// 2 - Si metodoUno lanza BException: A-B-E-G-H
 	// 3 - Si metodoDos lanza algo: A-B-C-G
-	// 4 - Si métodoUno lanza AException y metodoDos lanza algo: A-B-D-G
+	// 4 - Si metodoUno lanza AException y metodoDos lanza algo: A-B-D-G
 	public static void main(String[] args) {
 		for (int i = 0; i < CASOS.length; i ++) {
 			res = "";
@@ -46,11 +46,25 @@ public class OrdenEjecucion {
 	}
 
 	private static void metodoUno(int caso) throws AException, BException {
-		// tu código aquí
+		switch (caso) {
+		case 1:
+		case 4:
+			throw new AException();
+		case 2:
+			throw new BException();
+		default:
+			// todo bien
+		}
 	}
 
 	private static void metodoDos(int caso) {
-		// tu código aquí
+		switch (caso) {
+		case 3:
+		case 4:
+			throw new RuntimeException();
+		default:
+			// todo bien
+		}		
 	}
 }
 
