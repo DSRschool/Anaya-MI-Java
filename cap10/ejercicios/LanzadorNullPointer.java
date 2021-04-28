0259311a0378258cf9a9032f709586b871192ed0
Mariona DSR <mariona.nadal@dsrroma.es>
2021-04-28 19:55:56 +0200

    14. Ejercicio 10.1 - Solución

--- cap10/ejercicios/LanzadorNullPointer.java
+++ cap10/ejercicios/LanzadorNullPointer.java
@@ -2,5 +2,8 @@ public class LanzadorNullPointer {
 
 	public static void lloron() {
 		// Inténtalo aquí: escribe cualquier código que lance una NullPointerException 
+		String saludo = null;
+		saludo.toUpperCase();
+		System.out.println(saludo);
 	}
 }
