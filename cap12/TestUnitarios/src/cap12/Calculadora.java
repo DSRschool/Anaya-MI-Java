ff430f9837924fd96d603af06d06352e6c04f159
Mariona DSR <mariona.nadal@dsrroma.es>
2021-04-28 20:43:32 +0200

    1. Esqueleto de un test unitario

---
+++ cap12/TestUnitarios/src/cap12/Calculadora.java
@@ -0,0 +1,11 @@
+package cap12;
+
+public class Calculadora {
+	public static int suma(int a, int b) {
+		return a + b;
+	}
+
+	public static int resta(int a, int b) {
+		return a - b;
+	}
+}
---
+++ cap12/TestUnitarios/test/cap12/CalculadoraTest.java
@@ -0,0 +1,18 @@
+package cap12;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	@Test
+	void testSuma() {
+		fail("No implementado aun");
+	}
+
+	@Test
+	void testResta() {
+		fail("No implementado aun");
+	}
+}
