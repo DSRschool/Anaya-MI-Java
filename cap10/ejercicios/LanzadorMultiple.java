e4e7ef2115e6651008354a35886a000f045afec2
Mariona DSR <mariona.nadal@dsrroma.es>
2021-04-28 19:45:17 +0200

    10. Ejercicio 10.2

---
+++ cap10/ejercicios/LanzadorMultiple.java
@@ -0,0 +1,17 @@
+public class LanzadorMultiple {
+
+	public static void queMeSalgo() {
+		// Inténtalo aquí: escribe cualquier código 
+		// que lance una IndexOutOfBoundsException 
+	}
+
+	public static void malditaCasta() {
+		// Inténtalo aquí: escribe cualquier código 
+		// que lance una ClassCastException 
+	}
+
+	public static void siemprePositivoNuncaNegativo() {
+		// Inténtalo aquí: escribe cualquier código 
+		// que lance una NegativeArraySizeException
+	}
+}
