d2a712e15db4af4fe776ade63acaef672bc332eb
Mariona DSR <mariona.nadal@dsrroma.es>
2021-04-28 19:56:17 +0200

    15. Ejercicio 10.2 - Solución

--- cap10/ejercicios/LanzadorMultiple.java
+++ cap10/ejercicios/LanzadorMultiple.java
@@ -3,15 +3,22 @@ public class LanzadorMultiple {
 	public static void queMeSalgo() {
 		// Inténtalo aquí: escribe cualquier código 
 		// que lance una IndexOutOfBoundsException 
+		String[] lista = {"uno", "dos"};
+		String tercero = lista[2];
+		System.out.println(tercero);
 	}
 
 	public static void malditaCasta() {
 		// Inténtalo aquí: escribe cualquier código 
 		// que lance una ClassCastException 
+		Number numero = new Integer(3);
+		Double decimal = (Double) numero;
+		System.out.println(decimal);
 	}
 
 	public static void siemprePositivoNuncaNegativo() {
 		// Inténtalo aquí: escribe cualquier código 
 		// que lance una NegativeArraySizeException
+		String[] lista = new String[-2];
 	}
 }
