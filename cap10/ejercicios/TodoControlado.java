55f4fb70d55c2a91f5e5ba3d38925b08c31e3dd5
Mariona DSR <mariona.nadal@dsrroma.es>
2021-04-28 19:57:02 +0200

    16. Ejercicio 10.3 - Solución

--- cap10/ejercicios/TodoControlado.java
+++ cap10/ejercicios/TodoControlado.java
@@ -1,6 +1,10 @@
+import java.io.UnsupportedEncodingException; // No olvides importar la clase de la excepción!
+
 public class TodoControlado {
 
-	public static int cuentaBytes() {
+	// Como getBytes puede lanzar una UnsupportedEncodingException
+	// necesito declararlo con "throws"
+	public static int cuentaBytes() throws UnsupportedEncodingException {
 		String s = "Me ilusiona tener las excepciones controladas";
 		byte[] misBytes = s.getBytes("encodingQueNoExiste");
 		return misBytes.length;
