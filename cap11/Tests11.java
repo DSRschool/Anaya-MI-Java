50ec6ac884137db42691e760e757625f0bca5bbf
Mariona DSR <mariona.nadal@dsrroma.es>
2021-04-28 20:18:52 +0200

    1. Test

---
+++ cap11/Tests11.java
@@ -0,0 +1,14 @@
+
+public class Tests11 {
+	public static void main(String[] args) {
+		metodo(1);
+		metodo(2);
+	}
+
+	private static void metodo(int n) {
+		System.out.println("a" + n);
+		System.out.println("b" + n);
+		System.out.println("c" + n);
+		
+	}
+}
