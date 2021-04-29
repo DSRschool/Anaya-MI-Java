4b251a2db6c3da119c22cfcea511127b7890f0e0
Mariona DSR <mariona.nadal@dsrroma.es>
2021-04-29 20:40:38 +0200

    2. Validaciones

--- cap14/recordsManager/src/main/java/cap14/recordsManager/RecordsManager.java
+++ cap14/recordsManager/src/main/java/cap14/recordsManager/RecordsManager.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 
 import org.apache.logging.log4j.LogManager;
@@ -19,26 +20,35 @@ public class RecordsManager {
 	private static final int MIN_NAME_LENGTH = 6;
 	private static final int MIN_SCORE = 1000;
 
+	private static Random r = new Random();
+
 	public static void main(String[] args) {
-		// usaremos un map (o diccionario) para guardar los datos leídos
-		// nombre como clave y puntuación como contenido
 		Map<String, Integer> players = new HashMap<>();
 
 		/// (1) Leer fichero de entrada
 		Scanner s = new Scanner(new File(INPUT_FILE));
-		// mientras tenga más lineas
 		while (s.hasNextLine()) {
-			// cogemos la siguiente línea
 			String line = s.nextLine();
-			// y la troceamos por los espacios
 			String[] data = line.split(" ");
-			// el primer fragmento será el nombre del jugador
 			String name = data[0];
-			// y el segundo fragmento su puntuación
 			int score = Integer.parseInt(data[1]);
-			// comprobamos que todos los datos del jugador son correctos
-			validatePlayer(name, score);
-			// añadimos este jugador al diccionario
+			/// (7) Validamos el nombre
+			try {
+				validateName(name);
+			} catch (PlayerNameTooShortException pntse) {
+				logger.warn(pntse.getMessage());
+				name = generateNewName(name);
+				System.out.println("El nuevo nombre de usuario es " + name);
+			}
+			/// (8) Validamos la puntuación
+			// los tratamos por separado para poder validar la puntuación
+			// después de haber arreglado el nombre
+			try {
+				validateScore(name, score);
+			} catch (ScoreTooLowException stle) {
+				logger.error(stle.getMessage());
+				continue; // nos saltamos este jugador, vamos a la siguiente línea
+			}
 			players.put(name, score);
 			logger.info("Línea tratada correctamente: " + name + " - " + score);
 		}
@@ -51,25 +61,40 @@ public class RecordsManager {
 
 		/// (3) Pedir confirmación al usuario
 		System.out.println("¿Son correctos? [S]i/[N]o");
-		// leemos la respuesta del usuario de la entrada estándar
 		Scanner sConsole = new Scanner(System.in);
 		String answer = sConsole.next();
-		// y comprobamos si es afirmativa
 		boolean confirmed = answer.equalsIgnoreCase("S");
 
 		/// (4) Escribir a fichero
 		if (confirmed) {
 			System.out.println("Procedemos al volcado de datos del fichero...");
-			// abrimos el fichero de salida para escribir en él
 			FileOutputStream fos = new FileOutputStream(OUTPUT_FILE);
-			// por cada uno de los jugadores en nuestro diccionario
 			for (String name : players.keySet()) {
-				// escribimos una línea en el fichero de salida
 				fos.write((name + " " + players.get(name) + "\n").getBytes());
 			}
 		}
 	}
 
-	private static void validatePlayer(String name, int score) {
+	/// (5) Validamos la longitud del nombre
+	private static void validateName(String name) throws PlayerNameTooShortException {
+		if (name.length() < MIN_NAME_LENGTH) {
+			throw new PlayerNameTooShortException(name);
+		}
+	}
+
+	/// (6) Validamos la puntuación mínima
+	private static void validateScore(String name, int score) throws ScoreTooLowException {
+		if (score < MIN_SCORE) {
+			throw new ScoreTooLowException(name, score);
+		}
+	}
+
+	private static String generateNewName(String name) {
+		int randomSize = MIN_NAME_LENGTH - name.length();
+		for (int i = 0; i < randomSize; i++) {
+			int randomNum = r.nextInt(10);
+			name += randomNum; // añadimos un número más al final
+		}
+		return name;
 	}
 }
