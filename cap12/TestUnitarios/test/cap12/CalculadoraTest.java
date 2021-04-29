457b19371fc296c84a35edaf174563a99e77b64c
Mariona DSR <mariona.nadal@dsrroma.es>
2021-04-29 17:44:44 +0200

    5. Elección de los test unitarios adecuados (2)

--- cap12/TestUnitarios/test/cap12/CalculadoraTest.java
+++ cap12/TestUnitarios/test/cap12/CalculadoraTest.java
@@ -7,21 +7,151 @@ import org.junit.jupiter.api.Test;
 class CalculadoraTest {
 
 	@Test
-	void testSuma() {
+	void testSuma1() {
 		int res = Calculadora.suma(2, 3);
 		assertEquals(5, res);
 	}
 
 	@Test
-	void testResta() {
+	void testSuma2() {
+		int algo = 8;
+		int res = Calculadora.suma(0, algo);
+		assertEquals(algo, res);
+	}
+
+	@Test
+	void testSuma3() {
+		int algo = 7;
+		int res = Calculadora.suma(algo, 0);
+		assertEquals(algo, res);
+	}
+
+	@Test
+	void testSuma4() {
+		int res = Calculadora.suma(0, 0);
+		assertEquals(0, res);
+	}
+
+	@Test
+	void testSuma5() {
+		int algo = 3;
+		int res = Calculadora.suma(algo, -algo);
+		assertEquals(0, res);
+	}
+
+	@Test
+	void testSuma6() {
+		int algo = 5;
+		int res = Calculadora.suma(-algo, algo);
+		assertEquals(0, res);
+	}
+
+	@Test
+	void testSuma7() {
+		int a = 2;
+		int b = 3;
+		assertEquals(Calculadora.suma(a, b), Calculadora.suma(b, a));
+	}
+
+	@Test
+	void testResta1() {
+		int res = Calculadora.resta(8, 3);
+		assertEquals(5, res);
+	}
+
+	@Test
+	void testResta2() {
+		int algo = 3;
+		int res = Calculadora.resta(0, algo);
+		assertEquals(-algo, res);
+	}
+
+	@Test
+	void testResta3() {
+		int algo = 8;
+		int res = Calculadora.resta(algo, 0);
+		assertEquals(algo, res);
+	}
+
+	@Test
+	void testResta4() {
 		int res = Calculadora.resta(0, 0);
 		assertEquals(0, res);
 	}
 
 	@Test
-	void testMultiplica() {
+	void testResta5() {
+		int algo = 7;
+		int res = Calculadora.resta(algo, algo);
+		assertEquals(0, res);
+	}
+
+	@Test
+	void testResta6() {
+		int algo = 7;
+		int res = Calculadora.resta(algo, -algo);
+		assertEquals(2 * algo, res);
+	}
+
+	@Test
+	void testResta7() {
+		int a = 3;
+		int b = 2;
+		assertNotEquals(Calculadora.resta(a, b), Calculadora.resta(b, a));
+	}
+
+	@Test
+	void testMultiplica1() {
 		int res = Calculadora.multiplica(2, 3);
 		assertEquals(6, res);
+	}
+
+	@Test
+	void testMultiplica2() {
+		int algo = 9;
+		int res = Calculadora.multiplica(algo, 1);
+		assertEquals(algo, res);
+	}
+
+	@Test
+	void testMultiplica3() {
+		int algo = 9;
+		int res = Calculadora.multiplica(1, algo);
+		assertEquals(algo, res);
+	}
+
+	@Test
+	void testMultiplica4() {
+		int res = Calculadora.multiplica(1, 1);
+		assertEquals(1, res);
+	}
+
+	@Test
+	void testMultiplica5() {
+		int algo = 4;
+		int res = Calculadora.multiplica(algo, 0);
+		assertEquals(0, res);
+	}
+
+	@Test
+	void testMultiplica6() {
+		int algo = 4;
+		int res = Calculadora.multiplica(0, algo);
+		assertEquals(0, res);
+	}
+
+	@Test
+	void testMultiplica7() {
+		int res = Calculadora.multiplica(0, 0);
+		assertEquals(0, res);
+	}
+
+	@Test
+	void testMultiplica8() {
+		assertTrue(Calculadora.multiplica(1, 1) > 0);
+		assertTrue(Calculadora.multiplica(-1, -1) > 0);
+		assertTrue(Calculadora.multiplica(1, -1) < 0);
+		assertTrue(Calculadora.multiplica(-1, 1) < 0);
 	}
 
 	@Test
