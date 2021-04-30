a1f75442f96ea96bf5f6b48dde56120e9055138c
Mariona DSR <mariona.nadal@dsrroma.es>
2021-04-30 17:50:10 +0200

    7. Ejercicio 15.7 (Solución - Paso 1)

---
+++ cap15/Estructuras/src/Ejercicio15_04.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Ejercicio15_04a {
+	private static final Random RND = new Random();
+	private static final int NUM_LETRAS = 26;
+	private static final char PRIMERA_LETRA = 'a';
+
+	public static void main(String[] args) {
+		System.out.println(generaSilaba());
+	}
+	
+	private static String generaSilaba() {
+		return generaLetra() + generaLetra();
+	}
+	
+	private static String generaLetra() {
+		char c = (char) (PRIMERA_LETRA + RND.nextInt(NUM_LETRAS));
+		return Character.toString(c);
+	}
+}
