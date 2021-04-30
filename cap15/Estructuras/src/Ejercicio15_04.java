d6dea0111400cd8566d4d8e104a92d59b953f04c
Mariona DSR <mariona.nadal@dsrroma.es>
2021-04-30 17:50:58 +0200

    8. Ejercicio 15.4 (Solución - Paso 2)

--- cap15/Estructuras/src/Ejercicio15_04.java
+++ cap15/Estructuras/src/Ejercicio15_04.java
@@ -1,12 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Ejercicio15_04 {
 	private static final Random RND = new Random();
 	private static final int NUM_LETRAS = 26;
 	private static final char PRIMERA_LETRA = 'a';
+	private static final int TAM = 1000;
 
 	public static void main(String[] args) {
-		System.out.println(generaSilaba());
+		List<String> lista = generaLista();
+		System.out.println(lista);
+	}
+
+	private static List<String> generaLista() {
+		List<String> lista = new ArrayList<>(TAM);
+		for (int i = 0; i < TAM; i++) {
+			lista.add(generaSilaba());
+		}
+		return lista;
 	}
 	
 	private static String generaSilaba() {
