63139192feeffc3e6d02c4ebbddad108191b511d
Mariona DSR <mariona.nadal@dsrroma.es>
2021-05-01 09:36:02 +0200

    3. Entidad Pedido

---
+++ cap16/gestor/src/main/java/cap16/orm/gestor/Pedido.java
@@ -0,0 +1,47 @@
+package cap16.orm.gestor;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedido")
+public class Pedido {
+	@Column(name = "id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(name = "referencia")
+	private String referencia;
+	@Column(name = "fecha")
+	private LocalDateTime fecha;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getReferencia() {
+		return referencia;
+	}
+
+	public void setReferencia(String referencia) {
+		this.referencia = referencia;
+	}
+
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDateTime fecha) {
+		this.fecha = fecha;
+	}
+}
