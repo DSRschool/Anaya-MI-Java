048ecfd806bf97d3bce9b052fd817a20c4035586
Mariona DSR <mariona.nadal@dsrroma.es>
2021-05-03 11:12:52 +0200

    0. Proyecto Eclipse

---
+++ cap16/gestor/src/main/java/cap16/orm/gestor/App.java
@@ -0,0 +1,13 @@
+package cap16.orm.gestor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
