f21649639a30661681faf61f9103e138df78263c
Mariona DSR <mariona.nadal@dsrroma.es>
2021-05-03 16:56:16 +0200

    17. El DAO de Albaran

---
+++ cap16/gestor/src/main/java/cap16/orm/gestor/dao/AlbaranDao.java
@@ -0,0 +1,9 @@
+package cap16.orm.gestor.dao;
+
+import cap16.orm.gestor.Albaran;
+
+public class AlbaranDao extends AbstractDao<Albaran> {
+	public AlbaranDao() {
+		setClazz(Albaran.class);
+	}
+}
